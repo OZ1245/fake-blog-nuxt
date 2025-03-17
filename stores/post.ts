c019d1265853ff9c51f624b0870e9d48bae5e3db
Hope Halfbreed <boldyrevanadezhda@gmail.com>
2025-03-18 00:38:19 +0700

    тест сборки

--- stores/post.ts
+++ stores/post.ts
@@ -36,6 +36,7 @@ export const usePostStore = defineStore('postStore', () => {
   const fetchPosts = async <T>(): Promise<T> => {
     try {
       const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+      console.log(response);
       return await response.json() as T;
     } catch (error) {
       throw error;
